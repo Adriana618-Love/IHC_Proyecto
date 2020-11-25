7eaacd99a8636657687932709a00886f031230a5
Adriana618-Love <comofuncionaelmundoporalonso@gmail.com>
2020-11-25 09:58:53 -0500

    separación de escenas

---
+++ Assets/Scripts/Globo/GloboSlave.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.SceneManagement;
+
+public class GloboSlave : MonoBehaviour
+{
+    //server manager
+    public GameObject serverManager_;
+    private serverManager server;
+
+    public float speed;
+    public Vector2 direction;
+    public Sprite explosion;
+    public GameObject pausa_object;
+    public float minY;
+    public Vector2 initPos;
+
+    public float static_speed;
+
+    [SerializeField]
+    public screencapture screen_capture;
+
+    public AudioSource[] sounds;
+    public AudioSource trampolinAudio;
+    public AudioSource estallandoAudio;
+    private Animator animator;
+
+    private int _status; /*-1 => Exploto, 0 => Nada, 1 => Rebote*/
+
+    private KeywordRecognizer _keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        sounds = GetComponents<AudioSource>();
+        estallandoAudio = sounds[0];
+        trampolinAudio = sounds[1];
+        animator = this.GetComponent<Animator>();
+        //static_speed = Configs.configuraciones.speed_globo;
+
+        //server manager
+        server = serverManager._server_;
+    }
+
+    void Start()
+    {
+        initPos = this.transform.position;
+        direction = new Vector2(4, -4);
+        speed = static_speed;
+    }
+
+    public void Left_()
+    {
+        transform.Translate(-2, 0, 0);
+    }
+
+    public void Right_()
+    {
+        transform.Translate(2, 0, 0);
+    }
+
+    public IEnumerator Elevate()
+    {
+        speed = static_speed;
+        direction.y = 4;
+        yield return new WaitForSeconds(3f);
+        direction.y = -4;
+    }
+    public IEnumerator ElevateSimple()
+    {
+        if (this.transform.position.y < 0)
+        {
+            speed = static_speed;
+            direction.y = 4;
+            while (this.transform.position.y < -0.2f)
+            {
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+        else
+        {
+            speed = static_speed;
+            direction.y = -4;
+            while (this.transform.position.y > 0.2f)
+            {
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+        speed = 0f;
+    }
+
+    public void ES()
+    {
+        StartCoroutine("ElevateSimple");
+    }
+
+    public void Elevar_()
+    {
+        StartCoroutine("Elevate");
+    }
+
+    public void SetInit()
+    {
+        this.transform.position = initPos;
+        speed = static_speed;
+        direction.y = -4;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Spikes(Clone)")
+        {
+            Debug.Log("Explosion");
+            direction.y = direction.x = 0;
+            animator.SetTrigger("pum");
+            estallandoAudio.Play();
+            Invoke("SetInit", 1);
+            _status = -1;
+        }
+        else if (collision.gameObject.name == "Trampolin(Clone)")
+        {
+            Debug.Log("Rebota");
+            this.transform.position = new Vector2(transform.position.x, transform.position.y + 4);
+            trampolinAudio.Play();
+            _status = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x *= -1;
+        Vector2 movement = direction.normalized * speed * Time.deltaTime;
+        transform.Translate(movement);
+        if (transform.position.y <= minY) speed = 0;
+
+    }
+}
---
+++ Assets/Scripts/Ventilador/VentiladorSlave.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.SceneManagement;
+
+public class VentiladorSlave : MonoBehaviour
+{
+    //server manager
+    public GameObject serverManager_;
+    private serverManager server;
+
+    public float speed;
+    public Vector2 direction;
+    public Sprite explosion;
+    public GameObject pausa_object;
+    public float minY;
+    public Vector2 initPos;
+
+    public float static_speed;
+
+    [SerializeField]
+    public screencapture screen_capture;
+
+    private int _status; /*-1 => Exploto, 0 => Nada, 1 => Rebote*/
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        initPos = this.transform.position;
+        direction = new Vector2(4, -4);
+        speed = static_speed;
+        //server manager
+        server = serverManager._server_;
+    }
+
+    public void GirarAntihorario_()
+    {
+        transform.Rotate(Vector3.back * 90);
+    }
+
+    public void GirarHorario_()
+    {
+        transform.Rotate(Vector3.forward * 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
